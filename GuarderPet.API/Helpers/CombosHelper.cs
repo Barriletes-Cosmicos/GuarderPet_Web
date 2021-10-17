a11842e249c6749f092fc55ebabbc9300d3e2c11
luucasg <69911967+luucasg@users.noreply.github.com>
2021-10-17 17:48:48 -0500

    Helpers

---
+++ GuarderPet.API/Helpers/CombosHelper.cs
@@ -0,0 +1,85 @@
+﻿using GuarderPet.API.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GuarderPet.API.Helpers
+{
+    public class CombosHelper : ICombosHelper
+    {
+        private readonly DataContext _context;
+
+        public CombosHelper(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<SelectListItem> GetComboBreeds()
+        {
+            List<SelectListItem> list = _context.Breeds.Select(x => new SelectListItem
+            {
+                Text = x.BreedTittle,
+                Value = $"{x.Id }",
+            }).OrderBy(x => x.Text).ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione una marca...]",
+                Value = "0"
+            });
+
+            return list;
+        }
+
+        public IEnumerable<SelectListItem> GetComboPetServices()
+        {
+            List<SelectListItem> list = _context.PetServices.Select(x => new SelectListItem
+            {
+                Text = x.ServiceDetail,
+                Value = $"{x.Id }",
+            }).OrderBy(x => x.Text).ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione un procedimiento...]",
+                Value = "0"
+            });
+
+            return list;
+        }
+
+        public IEnumerable<SelectListItem> GetComboDocumentTypes()
+        {
+            List<SelectListItem> list = _context.DocumentTypes.Select(x => new SelectListItem
+            {
+                Text = x.Type,
+                Value = $"{x.Id }",
+            }).OrderBy(x => x.Text).ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione un tipo de documento...]",
+                Value = "0"
+            });
+
+            return list;
+        }
+
+        public IEnumerable<SelectListItem> GetComboPetTypes()
+        {
+            List<SelectListItem> list = _context.PetTypes.Select(x => new SelectListItem
+            {
+                Text = x.Type,
+                Value = $"{x.Id }",
+            }).OrderBy(x => x.Text).ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione un tipo de vehiculo...]",
+                Value = "0"
+            });
+
+            return list;
+        }
+    }
+}
---
+++ GuarderPet.API/Helpers/ICombosHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace GuarderPet.API.Helpers
+{
+    public interface ICombosHelper
+    {
+        IEnumerable<SelectListItem> GetComboDocumentTypes();
+        IEnumerable<SelectListItem> GetComboPetServices();
+        IEnumerable<SelectListItem> GetComboPetTypes();
+        IEnumerable<SelectListItem> GetComboBreeds();
+    }
+}
