abdd741474401b70437520723646d3a0b3574a44
Santiago <osorio1708@gmail.com>
2021-10-22 23:01:23 -0500

    Change User Menu

--- GuarderPet.API/wwwroot/js/site.js
+++ GuarderPet.API/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+
+    $('#userMenu').css("display", "none");
+    $('#userIcon').click(() => {
+
+        if ($('#userMenu').css("display") == "none") {
+            $('#userMenu').css("display", "flex");
+        }
+        else{
+            $('#userMenu').css("display", "none");
+        }
+    });
+
+});
