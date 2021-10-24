42c67d9717aaf82bed1e81c30e471c66ccdcbbce
Santiago Osorio <osorio1708@gmail.com>
2021-10-24 17:09:36 -0500

    Hor Fix 2

---
+++ GuarderPet.API/Helpers/IPlaceHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuarderPet.API.Helpers
+{
+    public interface IPlaceHelper
+    {
+    }
+}
